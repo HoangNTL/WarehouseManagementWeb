d54b7c75237b49f3460b5b6f18e9042eb8a94c7d
Hoangcutedz <hoangn2422@gmail.com>
2025-04-08 11:11:35 +0700

    connect db, add model

---
+++ WarehouseManagementWeb/Controllers/ImportInvoiceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WarehouseManagementWeb.Models;
+
+namespace WarehouseManagementWeb.Controllers
+{
+    public class ImportInvoiceController : Controller
+    {
+        DBDataContext db = new DBDataContext();
+        // GET: ImportInvoice
+        public ActionResult Index()
+        {
+            var importInvoices = db.ImportInvoices.ToList();
+            return View(importInvoices);
+        }
+    }
+}
